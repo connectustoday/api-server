3aaaea13bec659c30b58a94741d48801108896fd
Seshpenguin <seshan10@me.com>
2018-08-22 18:52:59 -0400

    Added Mongo

--- src/routes/auth/authRoutes.ts
+++ src/routes/auth/authRoutes.ts
@@ -1,23 +1,26 @@
 // Authentication API Routes
+
+// API Version 1
 import * as express from "express";
 
 export class AuthRoutes {
     public routes(app): void {
 
-        app.get('/auth', (req, res) => res.send('Authentication Endpoint - Invalid Request'));
+        app.get('/v1/auth', (req, res) => res.send('Authentication Endpoint - Invalid Request'));
 
         /* ~~ Registration Endpoint ~~ */
-        app.get('/auth/register', (req, res) => res.send('Invalid Request')); // Client should not GET this endpoint.
+        app.get('/v1/auth/register', (req, res) => res.send('Invalid Request')); // Client should not GET this endpoint.
 
-        app.post('/auth/register', function (req, res) {
+        app.post('/v1/auth/register', function (req, res) {
             res.send("Stub")
         });
 
         /* ~~ Sign In Endpoint ~~ */
-        app.get('/auth/signin', (req, res) => res.send('Invalid Request')); // Client should not GET this endpoint.
+        app.get('/v1/auth/signin', (req, res) => res.send('Invalid Request')); // Client should not GET this endpoint.
 
-        app.post('/auth/signin', function (req, res) {
+        app.post('/v1/auth/signin', function (req, res) {
             res.send("Stub")
+            // retrieve the JSON using body-parser (req.body)
         });
     }
 }
