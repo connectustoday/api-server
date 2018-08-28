04f2b60bbb52f524d7b6b9e86c0316ab8f16d199
EspiDev <espidev@gmail.com>
2018-08-28 11:18:44 -0400

    Added minimal theme

--- src/routes/routes.ts
+++
@@ -1,7 +0,0 @@
-import * as express from "express";
-
-export class Routes {       
-    public routes(app): void {          
-        app.get('/', (req, res) => res.send('ConnectUS Backend API Server - Working!')); // Default (root) route.       
-    }
-}
