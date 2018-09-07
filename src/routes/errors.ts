99e4e81595e855ef29a6f44cc4fde24507cb9dfa
EspiDev <espidev@gmail.com>
2018-09-06 22:02:06 -0400

    Added response codes
    Signed-off-by: EspiDev <espidev@gmail.com>

--- src/routes/errors.ts
+++ src/routes/errors.ts
@@ -16,16 +16,18 @@
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
-import * as express from "express";
-
-export class Routes {
-    public routes(app): void {
-        app.get('/', (req, res) => res.send('ConnectUS Backend API Server - Working!')); // Default (root) route.
-
-        // TODO USE BEST PRACTICES: https://www.owasp.org/index.php/OWASP_Cheat_Sheet_Series
-
-        // TODO MOVE OUT OF THIS FILE
-        app.get('/v1/')
-    }
-}
+export const ok = "200 - OK",
+    created = "201 - Created",
+    accepted = "202 - Accepted",
+    badRequest = "400 - Bad Request",
+    unauthorized = "401 - Unauthorized",
+    forbidden = "403 - Forbidden",
+    notFound = "404 - Not Found",
+    methodNotAllowed = "405 - Method Not Allowed",
+    unacceptable = "406 - Unacceptable",
+    payloadTooLarge = "413 - Payload Too Large",
+    unsupportedMediaType = "415 - Unsupported Media Type",
+    tooManyRequests = "429 - Too Many Requests",
+    internalServerError = "500 - Internal Server Error",
+    notImplemented = "501 - Not Implemented",
+    serviceUnavailable = "503 - Service Unavailable";
