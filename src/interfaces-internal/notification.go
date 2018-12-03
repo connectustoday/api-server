c0fe779cdf746adb642a7912c6b655d89d29f711
EspiDev <espidev@gmail.com>
2018-12-03 14:00:04 -0500

    Adding interfaces

--- src/interfaces-internal/notification.go
+++ src/interfaces-internal/notification.go
@@ -1,1 +1,9 @@
 package interfaces_internal
+
+type INotification struct {
+	ID int64 `bson:"id"`
+	CreatedAt int64 `bson:"created_at"`
+	Type string `bson:"type"`
+	Content string `bson:"content"`
+	Account string `bson:"account"`
+}
--- src/interfaces-internal/organization.go
+++ src/interfaces-internal/organization.go
@@ -1,1 +1,14 @@
 package interfaces_internal
+
+type IValidations struct {
+	UserID string `bson:"user_id"`
+	ExperienceID string `bson:"experience_id"`
+}
+
+type IOrganization struct {
+	PreferredName string `bson:"preferred_name"`
+	IsVerified bool `bson:"is_verified"`
+	Opportunities []string `bson:"opportunities"`
+	OrgInfo IOrganizationProfile `bson:"org_info"`
+	ExperienceValidations []IValidations `bson:"experience_validations"`
+}
