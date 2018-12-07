8fbaf303a0b07c4cd53ed9bcebf6d1677120d339
EspiDev <espidev@gmail.com>
2018-12-06 22:14:34 -0500

    Add path to build script

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ go get -t github.com/globalsign/mgo
 go get -t github.com/dgrijalva/jwt-go
 go get -t golang.org/x/crypto/bcrypt
 go get -t github.com/gorilla/schema
-go build -o api-server .
+GOPATH=$GOPATH:$PWD/../../ go build -o api-server .
 mv ./api-server ../../bin
 cd ../../
