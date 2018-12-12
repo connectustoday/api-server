29858a587b396c022d3f5db408724b54b7ec8c13
EspiDev <espidev@gmail.com>
2018-12-12 16:04:34 -0500

    Added conversions for api account specific

--- src/interfaces-conv/convert-to-api.go
+++ src/interfaces-conv/convert-to-api.go
@@ -7,27 +7,63 @@ import (
 
 func ConvertToIAccountAPI(acc interfaces_internal.IAccount) interfaces_api.IAccountAPI {
 	return interfaces_api.IAccountAPI{
-		UserName: acc.UserName,
-		Email: acc.Email,
-		Avatar: acc.Avatar,
-		Header: acc.Header,
-		CreatedAt: acc.CreatedAt,
-		Type: acc.Type,
-		PostsCount: len(acc.Posts),
-		LikedCount: len(acc.Liked),
+		UserName:    acc.UserName,
+		Email:       acc.Email,
+		Avatar:      acc.Avatar,
+		Header:      acc.Header,
+		CreatedAt:   acc.CreatedAt,
+		Type:        acc.Type,
+		PostsCount:  len(acc.Posts),
+		LikedCount:  len(acc.Liked),
 		SharedCount: len(acc.Shared),
+	}
+}
+
+func ConvertToIUserAPI(acc interfaces_internal.IUser) interfaces_api.IUserAPI {
+	return interfaces_api.IUserAPI{
+		UserName:    acc.UserName,
+		Email:       acc.Email,
+		Avatar:      acc.Avatar,
+		Header:      acc.Header,
+		CreatedAt:   acc.CreatedAt,
+		Type:        acc.Type,
+		PostsCount:  len(acc.Posts),
+		LikedCount:  len(acc.Liked),
+		SharedCount: len(acc.Shared),
+		FirstName:   acc.FirstName,
+		MiddleName:  acc.MiddleName,
+		LastName:    acc.LastName,
+		Birthday:    acc.Birthday,
+		Gender:      acc.Gender,
+	}
+}
+
+func ConvertToIOrganizationAPI(acc interfaces_internal.IOrganization) interfaces_api.IOrganizationAPI {
+	return interfaces_api.IOrganizationAPI{
+		UserName:      acc.UserName,
+		Email:         acc.Email,
+		Avatar:        acc.Avatar,
+		Header:        acc.Header,
+		CreatedAt:     acc.CreatedAt,
+		Type:          acc.Type,
+		PostsCount:    len(acc.Posts),
+		LikedCount:    len(acc.Liked),
+		SharedCount:   len(acc.Shared),
+		PreferredName: acc.PreferredName,
+		IsVerified:    acc.IsVerified,
+		Opportunities: acc.Opportunities,
 	}
 }
 
 func ConvertToIAddressAPI(address interfaces_internal.IAddress) interfaces_api.IAddressAPI {
 	return interfaces_api.IAddressAPI{
-		Street: address.Street,
-		City: address.City,
-		Province: address.Province,
-		Country: address.Country,
+		Street:     address.Street,
+		City:       address.City,
+		Province:   address.Province,
+		Country:    address.Country,
 		PostalCode: address.PostalCode,
-		AptNumber: address.AptNumber,
-		GeoJSON: ConvertToIPointAPI(address.GeoJSON),
+		AptNumber:  address.AptNumber,
+		GeoJSON:    ConvertToIPointAPI(address.GeoJSON),
 	}
 }
 
@@ -48,52 +84,52 @@ func ConvertToIExperienceAPI(experience interfaces_internal.IExperience) interfa
 
 func ConvertToIPointAPI(point interfaces_internal.IPoint) interfaces_api.IPointAPI {
 	return interfaces_api.IPointAPI{
-		Type: point.Type,
+		Type:        point.Type,
 		Coordinates: point.Coordinates,
 	}
 }
 
 func ConvertToIValidationsAPI(v interfaces_internal.IValidations) interfaces_api.IValidationsAPI {
 	return interfaces_api.IValidationsAPI{
-		UserID: v.UserID,
+		UserID:       v.UserID,
 		ExperienceID: v.ExperienceID,
 	}
 }
 
 func ConvertToIUserProfileAPI(v interfaces_internal.IUserProfile) interfaces_api.IUserProfileAPI {
 	return interfaces_api.IUserProfileAPI{
-		Interests: v.Interests,
-		Biography: v.Biography,
-		Education: v.Education,
-		Quote: v.Quote,
+		Interests:        v.Interests,
+		Biography:        v.Biography,
+		Education:        v.Education,
+		Quote:            v.Quote,
 		CurrentResidence: v.CurrentResidence,
-		Certifications: v.Certifications,
+		Certifications:   v.Certifications,
 	}
 }
 
 func ConvertToIOrganizationProfileAPI(v interfaces_internal.IOrganizationProfile) interfaces_api.IOrganizationProfileAPI {
 	return interfaces_api.IOrganizationProfileAPI{
-		Mission: v.Mission,
-		Quote: v.Quote,
-		Address: ConvertToIAddressAPI(v.Address),
+		Mission:        v.Mission,
+		Quote:          v.Quote,
+		Address:        ConvertToIAddressAPI(v.Address),
 		AffiliatedOrgs: v.AffiliatedOrgs,
-		Interests: v.Interests,
+		Interests:      v.Interests,
 	}
 }
 
 func ConvertToIUserSettingsAPI(v interfaces_internal.IUserSettings) interfaces_api.IUserSettingsAPI {
 	return interfaces_api.IUserSettingsAPI{
 		AllowMessagesFromUnknown: v.AllowMessagesFromUnknown,
-		EmailNotifications: v.EmailNotifications,
-		IsFullNameVisible: v.IsFullNameVisible,
-		BlockedUsers: v.BlockedUsers,
+		EmailNotifications:       v.EmailNotifications,
+		IsFullNameVisible:        v.IsFullNameVisible,
+		BlockedUsers:             v.BlockedUsers,
 	}
 }
 
 func ConvertToIOrganizationSettingsAPI(v interfaces_internal.IOrganizationSettings) interfaces_api.IOrganizationSettingsAPI {
 	return interfaces_api.IOrganizationSettingsAPI{
 		AllowMessagesFromUnknown: v.AllowMessagesFromUnknown,
-		EmailNotifications: v.EmailNotifications,
-		IsNonprofit: v.IsNonprofit,
+		EmailNotifications:       v.EmailNotifications,
+		IsNonprofit:              v.IsNonprofit,
 	}
 }
